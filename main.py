e74203e7218b2d78913ad8aa32f8255851a9551c
Katherine Mayo <katherine.y.mayo@gmail.com>
2025-05-09 16:08:56 -0500

    wrote command line code interface to add to (including determine if text fields for notes and dye lots should be extended or overridden), remove from, and view (including selective column viewing) tables

--- main.py
+++ main.py
@@ -1,20 +1,189 @@
 import yarn_tables
 
-def view(table_names, filter=None):
-    pass
+# allows for viewing both meters and imperial measurements despite the general table settings (useful depending on pattern listing, etc.)
+def view(tables, table_name, use_metric=False):
+    if table_name == 'yarn':
+        all_cols = tables.yarns_table.columns.tolist()
+    elif table_name == 'stash':
+        all_cols = tables.stash_table.columns.tolist()
+    
+    # color and brand name are always displayed
+    all_cols.remove('BRAND NAME')
+    all_cols.remove('COLOR')
 
-def add(table_name):
-    pass
+    print('You may view a subset of the columns by inputting a comma separated list (no spaces after comma) of column names, or typing "all" to see the data for all columns.')
+    print('Columns available for viewing: ', all_cols)
+    to_view = input('Which columns of data would you like to view: ').upper()
+    to_view = to_view.split(',')
+    validated_to_view = []
 
-def check_validity(input_string):
-    pass
+    if to_view[0] == 'ALL':
+        validated_to_view = all_cols
+    else:
+    # check everything in to_view is valid input (ie actual columns)
+        for tv in to_view:
+            if tv not in all_cols:
+                print('Input of {0} does not match available column names. Please re-enter this desired column name or type "skip" to skip.'.format(tv))
+
+                while tv not in all_cols and tv != 'SKIP':
+                    tv = input('Column name: ').upper()
+        
+                if tv == 'SKIP':
+                    continue
+
+            validated_to_view.append(tv)
+    
+    if table_name == 'yarn':
+        print(tables.yarns_table[validated_to_view])
+    elif table_name == 'stash':
+        print(tables.stash_table[[validated_to_view]])
+    
+def add_yarn(tables, brand=None, color=None, use_metric=False):
+    correct = 'no'
+
+    while correct == 'no':
+        if brand == None:
+            brand = input('Brand Name: ').lower()
+        if color == None:
+            color = input('Color: ').lower()
+        mat = input('Material: ')
+        yweight = input('Yarn Weight: ')
+        if use_metric:
+            length = input('Length/Skein (ms): ')
+        else:
+            length = input('Length/Skein (yds): ')
+        gweight = input('Weight/Skein (g): ')
+        wash = input('Machine washable (yes or no): ')
+        notes = input('Additional notes: ').lower()
+
+        print('We have gathered the following information:')
+        print('----Brand Name: ', brand)
+        print('----Color: ', color)
+        print('----Material: ', mat)
+        print('----Yarn Weight: ', yweight)
+        print('----Length/Skein: ', length)
+        print('----Weight/Skein (g): ', gweight)
+        print('----Machine Washable: ', wash == 'yes')
+        print('----Notes: ', notes)
+        print()
+        correct = input('Is this correct? (yes/no): ').lower()
+    
+    to_add = {'BRAND NAME': brand, 'COLOR': color, 'MATERIAL': mat, 'YARN WEIGHT': int(yweight), 'WEIGHT/SKEIN (G)': float(gweight), 'MACHINE WASHABLE': wash == 'yes', 'NOTES': notes}
+    if use_metric:
+        to_add['LENGTH/SKEIN (MS)'] = float(length)
+        to_add['LENGTH/SKEIN (YDS)'] = None
+    else:
+        to_add['LENGTH/SKEIN (YDS)'] = float(length)
+        to_add['LENGTH/SKEIN (MS)'] = None
+
+    tables.add_to_yarn(to_add)
+
+# need to check whether something is in the yarn table first
+# add it to yarn table if it isn't
+def add_stash(tables, use_metric=False):
+    correct = 'no'
+
+    while correct == 'no':
+        brand = input('Brand Name: ').lower()
+        color = input('Color: ').lower()
+
+        # add to yarns if doesn't already exist
+        print('Checking if {0} {1} exists in yarns.'.format(brand, color))
+
+        if tables.check_exists(tables.yarns_table, brand, color) == -1:
+            print('This yarn is not yet logged in the Yarn table. We require some additional information to log the yarn.')
+            add_yarn(tables, brand, color, use_metric)
+
+        dye_lots = input('Dye Lots (comma separated): ')
+
+        weight = ''
+        length = ''
+        num_skeins = ''
+
+        while num_skeins == '' and weight == '' and length == '':
+            print('You must supply either number of skeins, length or weight of yarn. To skip one, simply leave the field blank and press enter.')
+            num_skeins = input('Number of Skeins: ')
+
+            if use_metric:
+                length = input('Total Length (ms): ')
+            else:
+                length = input('Total Length (yds): ')
+
+            weight = input('Total Weight (g): ')
+        
+        print('We have gathered the following information:')
+        print('----Brand name: ', brand)
+        print('----Color: ', color)
+        if num_skeins != '':
+            print('----# Skeins: ', num_skeins)
+        else:
+            print('----# Skeins: not supplied')
+        if length != '':
+            print('----Total Length: ', length)
+        else:
+            print('----Total Length: not supplied')
+        if weight != '':
+            print('----Total Weight (g): ', weight)
+        else:
+            print('----Total Weight (g): not supplied')
+        print('----Dye Lots: ', dye_lots)
+
+        correct = input('Is this information correct (yes/no): ').lower()
+    
+    to_add = {'BRAND NAME': brand, 'COLOR': color, 'DYE LOTS': dye_lots}
+    if weight != '':
+        to_add['TOTAL WEIGHT (G)'] = weight
+    if length != '' and not use_metric:
+        to_add['TOTAL LENGTH (YDS)'] = length
+    if length != '' and use_metric:
+        to_add['TOTAL LENGTH (MS)'] = length
+    if num_skeins != '':
+        to_add['# SKEINS'] = num_skeins
+    
+    tables.add_to_stash(to_add)
+
+def remove_from_stash(tables):
+    correct = 'no'
+
+    while correct == 'no':
+        brand_name = input('Brand Name: ' ).lower()
+        color = input('Color: ').lower()
+
+        print('We will remove the following entry from the stash: ')
+        print('----Brand Name: ', brand_name)
+        print('----Color: ', color)
+        correct = input('You are about to remove this yarn from the stash. Is this information correct (yes/no): ').lower()
+    
+    if tables.check_exists(tables.stash_table, brand_name, color) > -1:
+        tables.remove_table_entry(tables.stash_table, remove_brand=brand_name, remove_color=color)
+    else:
+        print('** Error: This yarn is not present in the stash **')
+
+def check_validity(input_string, table=False):
+    valid_tables = ['yarn', 'stash', 'back']
+    valid_inputs = ['exit', 'view', 'add', 'filter', 'remove']
+
+    if table:
+        while input_string not in valid_tables:
+            input_string = input('Which table (yarn, stash, back to return): ').lower()
+    else:
+        while input_string not in valid_inputs:
+            print('What would you like to do?')
+            print('----add: add an entry to a table')
+            print('----view: view a table')
+            print('----remove: remove an entry from the stash')
+            print('----exit: exit the program')
+            print()
+            input_string = input('Type desired action: ').lower()
+    
+    return input_string
 
 if __name__ == '__main__':
 
     """
     TO DO:
     --1) use functions to populate and work with databases [ done ]
-    --2) interactive component to read input from command line to work with databases
+    --2) interactive component to read input from command line to work with databases [ongoing]
     --3) develop front end UI to work with tables instead
     """
     set_metric = False
@@ -24,35 +193,43 @@ if __name__ == '__main__':
 
     ci_input = None
 
-    """
-    --view tables
-    ----just one
-    ----separately
-    ----multiple
-
-    --add to a table
-    ----specify which table
-    ----input requested information
-    ----same thing as modifying and deleting for now
-
-    --
-    """
-
-    valid_inputs = ['exit', 'view', 'add', 'filter']
-
     while ci_input != 'exit':
-        print('What would you like to do?')
-        print('----view: to view tables')
-        print('----add: to add to a table')
+        ci_input = check_validity(ci_input, table=False)
+        if ci_input == 'exit':
+            break
+        table = check_validity(None, table=True)
         print()
-
-        ci_input = input('Type desired action: ')
-
-        if ci_input == 'view':
+        
+        if ci_input == 'view' and table != 'back':
+            view(yt, table, set_metric)
+        elif ci_input == 'add' and table != 'back':
+            print()
+            print('**************************************************')
+            
+            if table == 'yarn':
+                add_yarn(yt)
+                print('**************************************************')
+                print()
+                yt.print_table(yt.yarns_table)
+            elif table == 'stash':
+                add_stash(yt)
+                print('**************************************************')
+                print()
+                yt.print_table(yt.stash_table)
+        elif ci_input == 'remove' and table != 'back':
+            # can only remove from stash
+            if table == 'stash':
+                pass
+            elif table == 'yarn':
+                print('** Can only remove from stash. **')
+        elif ci_input == 'filter' and table != 'back':
             pass
-        elif ci_input == 'add':
-            pass
 
+        ci_input = None
+        table = None
+        
+        print()
+        
 
 
     #yt.save_data()
--- testing.py
+++ testing.py
@@ -11,26 +11,26 @@ def test_yarn_table(yts, print_info=True):
     
     # expected: add yarn to table
     print('*** Adding Lion Brand black ***')
-    new_yarn = {'Brand Name': 'Lion Brand', 'Color': 'black', 'Material': 'polyester', 'Yarn Weight': 3, 'Length/Skein (yds)': 300, 'Length/Skein (ms)': None, 'Weight/Skein (g)': 100, 'Machine Washable': False, 'Notes': 'very dark'}
-    yts.add_to_yarn(new_yarn)
+    new_yarn = {'BRAND NAME': 'Lion Brand', 'COLOR': 'black', 'MATERIAL': 'polyester', 'YARN WEIGHT': 3, 'LENGTH/SKEIN (YDS)': 300, 'LENGTH/SKEIN (MS)': None, 'WEIGHT/SKEIN (G)': 100, 'MACHINE WASHABLE': False, 'NOTES': 'very dark'}
+    yts.add_to_yarn(new_yarn, testing=True)
 
     if print_info:
         yts.print_table(yts.yarns_table)
         print()
-
+    
     # expected: update the information
     print('*** Updating info for Lion Brand black ***')
-    new_yarn = {'Brand Name': 'Lion brand', 'Color': 'black', 'Material': 'polyester', 'Yarn Weight': 3, 'Length/Skein (yds)': 400, 'Length/Skein (ms)': None, 'Weight/Skein (g)': 150, 'Machine Washable': False, 'Notes': 'nice to work with'}
-    yts.add_to_yarn(new_yarn)
+    new_yarn = {'BRAND NAME': 'Lion brand', 'COLOR': 'black', 'MATERIAL': 'polyester', 'YARN WEIGHT': 3, 'LENGTH/SKEIN (YDS)': 400, 'LENGTH/SKEIN (MS)': None, 'WEIGHT/SKEIN (G)': 150, 'MACHINE WASHABLE': False, 'NOTES': 'nice to work with'}
+    yts.add_to_yarn(new_yarn, testing=True)
 
     if print_info:
         yts.print_table(yts.yarns_table)
         print()
-
+    
     # expected: add yarn to table
     print('*** Adding Caron Heart red **')
-    new_yarn = {'Brand Name': 'Caron Heart', 'Color': 'red', 'Material': '50% acyrlic, 50% cotton', 'Yarn Weight': 5, 'Length/Skein (yds)': 300, 'Length/Skein (ms)': 250, 'Weight/Skein (g)': 150, 'Machine Washable': True, 'Notes': ''}
-    yts.add_to_yarn(new_yarn)
+    new_yarn = {'BRAND NAME': 'Caron Heart', 'COLOR': 'red', 'MATERIAL': '50% acyrlic, 50% cotton', 'YARN WEIGHT': 5, 'LENGTH/SKEIN (YDS)': 300, 'LENGTH/SKEIN (MS)': 250, 'WEIGHT/SKEIN (G)': 150, 'MACHINE WASHABLE': True, 'NOTES': ''}
+    yts.add_to_yarn(new_yarn, testing=True)
 
     if print_info:
         yts.print_table(yts.yarns_table)
@@ -40,8 +40,9 @@ def test_yarn_table(yts, print_info=True):
     if not print_info:
         yts.print_table(yts.yarns_table)
         print()
-
-    print('---------- Testing Completed ----------')
+    
+    print('---------- Testing Yarn Table Completed ----------')
+    print()
 
 # test that stash table behaves as desired -- requires yarn table to be popualted to work
 def test_stash_table(yts):
@@ -50,45 +51,46 @@ def test_stash_table(yts):
 
     # expected: add to stash and fill in total weight (also total length in ms, but that won't be shown)
     print('*** Add lion brand black to stash -- should calculate the total weight')
-    new_stash = {'Brand Name': 'lion brand', 'Color': 'black', '# Skeins': 2, 'Total Length (yds)': 100, 'Total Length (ms)': None, 'Total Weight (g)': None, 'Dye Lots': 'xyz' }
-    yts.add_to_stash(new_stash)
+    new_stash = {'BRAND NAME': 'lion brand', 'COLOR': 'black', '# SKEINS': 2, 'TOTAL LENGTH (YDS)': 100, 'TOTAL LENGTH (MS)': None, 'TOTAL WEIGHT (G)': None, 'DYE LOTS': 'xyz' }
+    yts.add_to_stash(new_stash, testing=True)
 
     yts.print_table(yts.stash_table)
     print()
 
     # expected: update stash to increase # of skeins by adding duplicate (total length and number of skeins should increase)
     print('*** Increase amount of lion brand black in stash -- # skeins and length should increase as result')
-    new_stash = {'Brand Name': 'lion brand', 'Color': 'black', '# Skeins': None, 'Total Length (yds)': None, 'Total Length (ms)': None, 'Total Weight (g)': 500, 'Dye Lots': 'xyz, abc'}
-    yts.add_to_stash(new_stash)
+    new_stash = {'BRAND NAME': 'lion brand', 'COLOR': 'black', '# SKEINS': None, 'TOTAL LENGTH (YDS)': None, 'TOTAL LENGTH (MS)': None, 'TOTAL WEIGHT (G)': 500, 'DYE LOTS': 'xyz, abc'}
+    yts.add_to_stash(new_stash, testing=True)
 
     yts.print_table(yts.stash_table)
     print()
 
     # expected: add to stash something not in yarns, producing an error
     print('*** Add to stash something not in yarns ***')
-    new_stash = {'Brand Name': 'Malabrigo Rios', 'Color': 'gray', '# Skeins': 1, 'Total Length (yds)': None, 'Total Length (ms)': None, 'Total Weight (g)': None, 'Dye Lots': '' }
-    yts.add_to_stash(new_stash)
+    new_stash = {'BRAND NAME': 'Malabrigo Rios', 'COLOR': 'gray', '# SKEINS': 1, 'TOTAL LENGTH (YDS)': None, 'TOTAL LENGTH (MS)': None, 'TOTAL WEIGHT (G)': None, 'DYE LOTS': '' }
+    yts.add_to_stash(new_stash, testing=True)
 
     yts.print_table(yts.stash_table)
     print()
 
     # expected: update stash to decrease the # skeins by adding duplicate
     print('*** Decrease amount stashed of lion brand black ***')
-    new_stash = {'Brand Name': 'lion brand', 'Color': 'black', '# Skeins': None, 'Total Length (yds)': 150, 'Total Length (ms)': None, 'Total Weight (g)': None, 'Dye Lots': ''}
-    yts.add_to_stash(new_stash)
+    new_stash = {'BRAND NAME': 'lion brand', 'COLOR': 'black', '# SKEINS': None, 'TOTAL LENGTH (YDS)': 150, 'TOTAL LENGTH (MS)': None, 'TOTAL WEIGHT (G)': None, 'DYE LOTS': ''}
+    yts.add_to_stash(new_stash, testing=True)
 
     yts.print_table(yts.stash_table)
     print()
 
     # expected: remove from stash by adding duplicate with no skeins, length, or weight
     print('*** Remove from stash ***')
-    new_stash = {'Brand Name': 'lion brand', 'Color': 'black', '# Skeins': 0, 'Total Length (yds)': 0, 'Total Length (ms)': 0, 'Total Weight (g)': 0, 'Dye Lots': ''}
-    yts.add_to_stash(new_stash)
+    new_stash = {'BRAND NAME': 'lion brand', 'COLOR': 'black', '# SKEINS': 0, 'TOTAL LENGTH (YDS)': 0, 'TOTAL LENGTH (MS)': 0, 'TOTAL WEIGHT (G)': 0, 'DYE LOTS': ''}
+    yts.add_to_stash(new_stash, testing=True)
 
     yts.print_table(yts.stash_table)
     print()
 
-    print('----- Testing Completed -----')
+    print('----- Testing Stash Table Completed -----')
+    print()
 
 if __name__ == '__main__':
     
